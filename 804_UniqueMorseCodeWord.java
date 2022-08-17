86dc247c8af322b16d05b894e97104762a14eab4
agentgrey <himadrinayak2702@gmail.com>
2022-08-17 19:51:45 +0530

    17/08

---
+++ 804_UniqueMorseCodeWord.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+                String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        
+        HashSet<String> set = new HashSet<>();
+        for(int i=0; i<words.length; i++) {
+            String temp = "";
+            for(int j=0; j<words[i].length(); j++) {
+                temp += morse[words[i].charAt(j)-'a'];
+            }
+            set.add(temp);
+        }
+        
+        return set.size();
+
+    }
+}
